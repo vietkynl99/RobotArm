aab347b13c3cb1f85e9f4b7be54d3cc87406fda6
vietkynl99 <vietdohoang1999@gmail.com>
2024-05-29 11:48:01 +0700

    Delete unused command line

--- Core/Src/RobotArm.cpp
+++ Core/Src/RobotArm.cpp
@@ -84,60 +84,6 @@ bool onCommandPosition(string params)
     return false;
 }
 
-bool onCommandResetServo(string params)
-{
-    // for (int i = 0; i < SERVO_NUMS; i++)
-    // {
-    //     mServo[i]->reset();
-    // }
-    return true;
-}
-
-bool onCommandTune(string params)
-{
-    // float kp = 0, ki = 0, kd = 0;
-    // int ret = sscanf(params.c_str(), "%f %f %f", &kp, &ki, &kd);
-    // if (ret >= 1 && ret <= 3)
-    // {
-    //     println("PID tune: kp=%.2f ki=%.2f kd=%.2f", kp, ki, kd);
-    //     PidParams params{kp, ki, kd};
-    //     mServo[0]->tune(params);
-    //     return true;
-    // }
-    // else
-    // {
-    //     return false;
-    // }
-    return false;
-}
-
-bool onCommandEnable(string params)
-{
-    // if (params == "on")
-    // {
-    //     println("Servo is enabled");
-    //     for (int i = 0; i < SERVO_NUMS; i++)
-    //     {
-    //         mServo[i]->setEnable(true);
-    //     }
-    //     return true;
-    // }
-    // else if (params == "off")
-    // {
-    //     println("Servo is disabled");
-    //     for (int i = 0; i < SERVO_NUMS; i++)
-    //     {
-    //         mServo[i]->setEnable(false);
-    //     }
-    //     return true;
-    // }
-    // else
-    // {
-    //     return false;
-    // }
-    return false;
-}
-
 bool onCommandZeroDetect(string params)
 {
     if (!params.empty())
@@ -155,9 +101,6 @@ void setup(TIM_HandleTypeDef *htim1, TIM_HandleTypeDef *htim2, TIM_HandleTypeDef
     CommandLine::init();
     CommandLine::install("reboot", onCommandReboot, "reboot\t: reboot device");
     CommandLine::install("position", onCommandPosition, "position [value]\t: rotate the servo to position\r\nservo-position\t: get current position");
-    CommandLine::install("reset", onCommandResetServo, "reset\t: reset servo data");
-    CommandLine::install("tune", onCommandTune, "tune [kp] [ki] [kd]\t: set pid controller params");
-    CommandLine::install("enable", onCommandEnable, "enable [on/off]\t: turn on/off servo");
     CommandLine::install("zero-detect", onCommandZeroDetect, "zero-detect\t: Zero dectection");
 
     mController = new DeviceController(htim1, htim2, htim3, hspi);
